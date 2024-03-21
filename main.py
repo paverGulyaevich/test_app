a2abf6db48e4758d96023c511b5b202979c57688
Pavel Gulyaev <103142331+paverGulyaevich@users.noreply.github.com>
2024-03-21 14:50:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/matvey_pidar')
+async def matvey():
+    return "МАТВЕЙ ПИДАР И САСЕТ ХУЙ!!!! ЗНАЙТЕ ВСЕ!"
